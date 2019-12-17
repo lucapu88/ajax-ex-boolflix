cb7f0c36b618140b8a1f3fe19b52c158b1e69c61
lucapu88 <caputoluca88@gmail.com>
2019-12-17 14:31:13 +0100

    1-migliorato il layout. 2-stelline fatte

--- main.js
+++ main.js
@@ -5,9 +5,12 @@
 // 2. Titolo Originale
 // 3. Lingua
 // 4. Voto
+// Milestone 2:
+// Trasformiamo il voto da 1 a 10 decimale in un numero intero da 1 a 5, così da permetterci di stampare a schermo un numero di stelle piene che vanno da 1 a 5, lasciando le restanti vuote (troviamo le icone in FontAwesome).
+// Arrotondiamo sempre per eccesso all’unità successiva, non gestiamo icone mezze piene (o mezze vuote :P)
+// Trasformiamo poi la stringa statica della lingua in una vera e propria bandiera della nazione corrispondente, gestendo il caso in cui non abbiamo la bandiera della nazione ritornata dall’API (le flag non ci sono in FontAwesome).
+// Allarghiamo poi la ricerca anche alle serie tv. Con la stessa azione di ricerca dovremo prendere sia i film che corrispondono alla query, sia le serie tv, stando attenti ad avere alla fine dei valori simili (le serie e i film hanno campi nel JSON di risposta diversi, simili ma non sempre identici)
 
-//devo far si che la parola digitata nell'input corrisponda ad un film presente nell'api_key
-//se il film è presente mi devono apparire le info
 $(document).ready(function(){
   $('button').click(function(){ // al click sul pulsante cerca
     $('.searchReaults').remove(); //ad ogni nuova ricerca, si cancella la precedente
@@ -32,33 +35,40 @@ function trovaFilm() {
       url : 'https://api.themoviedb.org/3/search/movie',
       data : {
         'api_key' : API_KEY,
-        'query' : filmCercato
+        'query' : filmCercato,
+        'language' : 'it'
       },
       method : 'get',
       success : function(data) {
         var filmResults = data.results; //creo una variabile che mi prende l'array dei risultati dentro l'API
         for (var i = 0; i < filmResults.length; i++) { //vado a scorrere per tutta la lunghezza dell'array
           var titolo = filmResults[i].title //creo una var per il titolo del film
+          var votoNum = Math.round(filmResults[i].vote_average / 2); //creo una var che dimezza il voto e lo arrotonda
           if (titolo.toLowerCase().includes(filmCercato.toLowerCase())) { // se ciò che abbiamo digitato nell'input corrisponde ad titolo presente nell'API
            var context = { //creo la variabile con il contenuto che andrà nel template.
-              titolo : filmResults[i].title,
+              titolo : '<h2>' + filmResults[i].title + '</h2>',
               titolo_originale : filmResults[i].original_title,
               lingua : filmResults[i].original_language,
-              voto : filmResults[i].vote_average
+              voto : '<i class="fas fa-star"></i>'.repeat(votoNum), //ripete la stella per il numero del voto
+              voto_restante : '<i class="far fa-star"></i>'.repeat(5 - votoNum) //aggiunge la stella per il risultato di 5 meno il numero del voto
             }
             var risultatoRicerca = template_function(context); // utilizzando la funzione generata da handlebars, creo l'html finale
-            $('body').append(risultatoRicerca); // infine vado ad appendere nel container il mio template che si ripeterà fino alla lunghezza dell'array results contenuto nell'API
+            $('.searchReaults-container').append(risultatoRicerca); // infine vado ad appendere nel container il mio template che si ripeterà fino alla lunghezza dell'array results contenuto nell'API
             $('.searchMovie').val(''); //resetto l'input con una stringa vuota
           }
-          // if (filmResults.length == 0) { //se non c'è nessun film con il nome digitato
-          //   $('body').append('<h2>Nessun risultato trovato</h2>');
+          // if (data.total_results == 0) { //se non c'è nessun film con il nome digitato
+          //   $('.searchReaults-container').append('Nessun risultato trovato');
           //   console.log('oook');
           // }
         }
       },
       error : function() {
-
+        alert('error');
       }
     });
   }
+}
+
+function stampaRisultato(){
+
 }
